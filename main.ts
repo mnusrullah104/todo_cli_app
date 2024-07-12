78097a654b613f48e18f6361b4ce7f6cb7b6a807
mnusrullah104 <133938501+mnusrullah104@users.noreply.github.com>
2024-07-12 10:16:54 +0500

    first commit

---
+++ main.ts
@@ -0,0 +1,28 @@
+import inquirer from "inquirer"
+let todos=[];
+let condition=true;
+while(condition)
+
+{
+    let todosquestion=await inquirer.prompt(
+   [
+     {
+    name: "firstQuestion",
+    type:"input",
+    message:"What would you like to add in todos"
+},
+{
+    name:"secondQuestion",
+    type:"confirm",
+    message:"would you like to add more todos",
+    default:"true"
+}
+]
+);
+todos.push(todosquestion.firstQuestion);
+
+
+console.log(todos)
+condition=todosquestion.secondQuestion;
+}
+
